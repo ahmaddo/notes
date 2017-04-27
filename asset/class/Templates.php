e3f51eb1a030164b891c851064313e678d31500c
Ahmad2 <ahmad@newsletter2go.com>
2017-04-27 11:04:33 +0200

    Templates Class

---
+++ asset/class/Templates.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User: Ahmad
+ * Project notes
+ * Date: 27.04.2017
+ * Time: 10:56
+ */
+
+namespace notes;
+
+
+class Templates
+{
+
+    static public function getNoteTemplate()
+    {
+        return '
+        <div>
+            <form method="post" action="'.$_SERVER['PHP_SELF'].'" name="notesForm">
+                <label for="">Your new NoIt:</label>
+                <input type="text" autocomplete="off">
+                <input type="submit" value="Add">
+            </form>
+        </div>
+        ';
+    }
+}

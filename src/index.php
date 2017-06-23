404a99c0b31695cb5674f91722a658aa202ab606
ahmaddo <a.ahmad@outlook.de>
2017-06-23 17:46:04 +0200

    calling headers tag

--- src/index.php
+++ src/index.php
@@ -12,10 +12,10 @@ use \notes\Notes as Notes;
 use \notes\Templates as Templates;
 
 echo Templates::getHeaders();
-echo Templates::getNoteTemplate();
+echo Templates::getFormTemplate();
 $notes = new Notes();
 if(isset($_POST['content'])) {
-    var_dump($_POST);
+    //var_dump($_POST);
     $note = $_POST;
     $notes->addNote($note);
 }
